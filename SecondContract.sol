eedd75339625a592d92f4609d028b7c0b78a5fcd
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-03-21 21:39:10 -0600

    Update and rename SecondContract.rtf to SecondContract.sol

---
+++ SecondContract.sol
@@ -0,0 +1,52 @@
+pragma solidity ^0.5.5; 
+
+contract MyContract {
+    string public value = "myValue";
+    string public constant fix = "Cannot change";
+    bool public myBool = true;
+    int public myint = -2;
+    uint public peopleCount; 
+    uint public peopleCount2;
+    enum State {a, b, c}
+    State public state;
+    
+    struct Person {
+        uint id;
+        string firstName; 
+        string lastName;
+    }
+    
+    Person[] public people;
+    
+    mapping(uint => Person) public people2;
+    
+    function addPerson2(string memory _firstName, string memory _lastName) public {
+        peopleCount2 += 1;
+        people2[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+    
+     function addPerson(string memory _firstName, string memory _lastName) public {
+        peopleCount += 1;
+        people.push(Person(peopleCount, _firstName, _lastName));
+    }
+
+    constructor() public {
+        state = State.a;
+    }
+    
+    function activate() public {
+        state = State.b;
+    }
+    
+    function is_b() public view returns(bool) {
+        return state == State.b;
+    }
+    
+/*    function get() public view returns(string memory) {
+       return value; 
+    }*/
+    
+    function set(string memory _value) public {
+        value = _value;
+    }
+}
