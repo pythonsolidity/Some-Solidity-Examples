8ed969ac4092032a1a0fe1a537c0cec37e3288f5
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-03-21 21:36:41 -0600

    Update and rename FifthContract.rtf to FifthContract.sol

---
+++ FifthContract.sol
@@ -0,0 +1,32 @@
+pragma solidity ^0.5.5;
+
+contract ERC20Token {
+    string public name;
+    mapping(address => uint) public balances; 
+    
+    function mint() public {
+        balances[tx.origin] ++;
+    }    
+}
+
+
+
+contract MyContract {
+   
+    address payable wallet;
+    address payable token;
+    
+    constructor(address payable _token) public {
+        wallet = msg.sender;    
+        token = _token;
+    }
+    
+    function() external payable {
+        buytoken();
+    }
+    
+    function buytoken() public payable {
+        ERC20Token(address(token)).mint();
+        wallet.transfer(msg.value);
+    }
+}
