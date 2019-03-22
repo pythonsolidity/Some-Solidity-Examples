c711c5d1bcc1b4d3ce9de22b32116e76c740cfe7
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-03-21 21:41:15 -0600

    Update and rename ThirdContract.rtf to ThirdContract.sol

---
+++ ThirdContract.sol
@@ -0,0 +1,39 @@
+pragma solidity ^0.5.5;
+
+contract MyContract{
+    uint256 public peopleCount = 0;
+    mapping(uint => Person) public people;
+    address owner; 
+    
+    constructor() public {
+        owner = msg.sender;
+    }
+    
+    struct Person {
+        uint id; 
+        string firstName;
+        string lastName;
+    }
+    
+    modifier onlyOwner() {
+        require(msg.sender == owner);
+        _;
+    }
+    
+    modifier onlyWhileOpen() {
+        require(block.timestamp >= 1552965871);
+        _;
+    }
+
+
+    
+    function addPerson(
+        string memory _firstName, string memory _lastName) public onlyWhileOpen {
+        incrementCount();
+        people[peopleCount] = Person(peopleCount, _firstName, _lastName);
+    }
+    
+    function incrementCount() internal {
+        peopleCount += 1;
+    }
+}
