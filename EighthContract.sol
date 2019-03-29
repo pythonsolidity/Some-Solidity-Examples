ba72d4fff179e9549a2066640c95d5d474214366
Muhammad Ali Khan <48421445+pythonsolidity@users.noreply.github.com>
2019-03-28 18:51:51 -0600

    Update EighthContract.sol

--- EighthContract.sol
+++ EighthContract.sol
@@ -2,14 +2,24 @@ pragma solidity 0.5.1;
 
 contract MyContract {
      address payable public addr = address(uint160(address(this)));
+     address payable public owner;
      uint public balance;
      
+     
     constructor () payable public {
-        addr.transfer(msg.value);
+        require(msg.value > 10 ether);
+        owner = msg.sender;
+        //addr.transfer(msg.value);
         balance = addr.balance;
-    } 
+    }
+    
+    function money () payable public {
+        require(msg.value > 20 ether);
+        balance = addr.balance;
+    }
     
     function end () payable public {
+        require(msg.sender == owner);
         selfdestruct(msg.sender);
     }
 }
